491a108fc59339308bb2361a5d73feb3b61b0190
Matt Dolan <mdolan.code@gmail.com>
2021-03-17 07:34:53 -0700

    Create Hello and GoodBye ViewControllers.

---
+++ Coordinators/GoodByeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GoodByeViewController.swift
+//  Coordinators
+//
+//  Created by Matt Dolan External macOS on 2021-03-17.
+//
+
+import UIKit
+
+class GoodByeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
