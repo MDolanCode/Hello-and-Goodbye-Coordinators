e69a6832eb04b0b05ab56eabb724f2d67e276d25
Matt Dolan <mdolan.code@gmail.com>
2021-03-17 07:57:29 -0700

    Add backgroundColor to Hello and GoodBye buttons. Set width and height constraints for buttons.

--- Coordinators/ViewController.swift
+++ Coordinators/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController, Storyboarded {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        sayHelloButton.backgroundColor = .orange
+        sayGoodByeButton.backgroundColor = .purple
     }
     
     @IBAction func sayHelloPressed(_ sender: UIButton) {
